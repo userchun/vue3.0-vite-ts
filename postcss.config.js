7efbfe1c0b7cc46cb1d5dcccb81f9e2d919b8541
paris.shi <paris.shi@winabc.com>
2021-04-25 16:44:43 +0800

    feat:添加单元测试

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     'postcss-pxtorem': {
-      rootValue: 200, // Vant 官方根字体大小是 37.5
+      rootValue: 37.5, // Vant 官方根字体大小是 37.5
       propList: ['*'],
       selectorBlackList: ['.norem'], // 过滤掉.norem-开头的class，不进行rem转换
     },
--- src/tests/Test.spec.ts
+++ src/tests/Test.spec.ts
@@ -1,8 +1,8 @@
 import { mount } from '@vue/test-utils';
 import Test from '../views./views/Test.vue';
-import Login from '../views/Login/index.vue';
+
 test('Test.vue', async () => {
-  const wrapper = mount(Login);
+  const wrapper = mount(Test);
   expect(wrapper.html()).toContain('Unit Test Page');
   expect(wrapper.html()).toContain('count is: 0');
   await wrapper.find('button').trigger('click');
